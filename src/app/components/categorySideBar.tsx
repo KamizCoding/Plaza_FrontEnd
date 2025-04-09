4cc2793e049caa07e2d46014e8f67cb841ee6da8
Lukmaan Kamiss <m.lukmaankamiz@gmail.com>
2025-04-09 17:46:05 +0530

    Landing Page Setup

---
+++ src/app/components/categorySideBar.tsx
@@ -0,0 +1,8 @@
+export default function CategorySidebar() {
+    return (
+      <div className="h-64 bg-gray-200 flex items-center justify-center">
+        <h2 className="text-2xl font-semibold">CategorySideBar</h2>
+      </div>
+    );
+  }
+  
---
+++ src/app/components/footer.tsx
@@ -0,0 +1,8 @@
+export default function footer() {
+    return (
+      <div className="h-64 bg-gray-200 flex items-center justify-center">
+        <h2 className="text-2xl font-semibold">Footer</h2>
+      </div>
+    );
+  }
+  
